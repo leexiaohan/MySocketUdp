aa012d058b854f030ca3fa8b23152a79f522c810
xiaohan <leexiaohan2014@gmail.com>
2016-02-02 21:29:58 +0100

    delete port input and adapt it to the our system IOT

--- app/src/main/java/com/example/leexiaohan2014/mysocketudp/MainActivity.java
+++ app/src/main/java/com/example/leexiaohan2014/mysocketudp/MainActivity.java
@@ -3,36 +3,29 @@ package com.example.leexiaohan2014.mysocketudp;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 
 public class MainActivity extends ActionBarActivity {
 
     EditText ip;
-    EditText port;
-    EditText editText;
     TextView textListen;
-    EditText port2;
-    String ipaddress;
-    String portaddress;
-    String portaddress2;
+    String ipAddress;
+    static int portLocal = 8888;
+    static int portServer = 12345;
     String stringToSend;
-    String result;
+    //String result;
+    DatagramSocket socket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,39 +33,64 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         ip = (EditText) findViewById(R.id.ip);
-        port = (EditText) findViewById(R.id.port);
-        port2 = (EditText) findViewById(R.id.port2);
-        editText = (EditText) findViewById(R.id.editText);
         textListen = (TextView) findViewById(R.id.textListen);
-
-        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
+        //Switch relay1 = (Switch)findViewById(R.id.switch1);
+        //Switch relay2 = (Switch)findViewById(R.id.switch2);
+        textListen.setMovementMethod(ScrollingMovementMethod.getInstance());
+        //request----------------------------------------------------------------
+        findViewById(R.id.request).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                ipaddress = ip.getText().toString();
-                portaddress = port.getText().toString();
-                stringToSend = editText.getText().toString();
-                if (!((ipaddress.isEmpty())||(portaddress.isEmpty()))) {
+                ipAddress = ip.getText().toString();
+                stringToSend = "request";
+                textListen.append("send request to " + ipAddress + "\n");
+                if (!(ipAddress.isEmpty())) {
                     send();
+                    listen();
                 }
-                else {
-                    //text.append("-------IP or PORT empty-------\n");
-                }
+                //else
+                //   textListen.append("ip empty");
 
             }
         });
+        findViewById(R.id.relaySet).setOnClickListener(new View.OnClickListener() {
+            Switch relay1 = (Switch) findViewById(R.id.switch1);
+            Switch relay2 = (Switch) findViewById(R.id.switch2);
 
-
+            @Override
+            public void onClick(View v) {
+                ipAddress = ip.getText().toString();
+                if (relay1.isChecked() && relay2.isChecked()) {
+                    textListen.append("relay1,2 selected\n");
+                    stringToSend = "1";
+                } else if (!(relay1.isChecked() || relay2.isChecked())) {
+                    textListen.append("relay1,2 not selected\n");
+                    stringToSend = "2";
+                } else if ((!relay1.isChecked()) && relay2.isChecked()) {
+                    textListen.append("relay 1 not selected, relay2 selected\n");
+                    stringToSend = "4";
+                } else {
+                    textListen.append("relay1 selected, relay2 not selected\n");
+                    stringToSend = "3";
+                }
+                stringToSend = "relay_state," + stringToSend;
+                textListen.append("send relay state to " + ipAddress + "\n");
+                if (!(ipAddress.isEmpty())) {
+                    send();
+                    //listen();
+                }
+            }
+        });
+/*
         findViewById(R.id.listen).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                portaddress2 = port2.getText().toString();
+                //portaddress2 = port2.getText().toString();
                 listen();
                 //textListen.append("text received from server" + result);
             }
         });
-
+*/
 
     }
 
@@ -108,19 +126,16 @@ public class MainActivity extends ActionBarActivity {
             protected Void doInBackground(Void... params) {
                 try {
                     //首先创建一个DatagramSocket对象
-                    DatagramSocket socket=null;
-                    if (socket == null){
-                        socket = new DatagramSocket(12222);
-                        socket.setReuseAddress(true);
-                        //socket.bind(new InetSocketAddress(12222));
-                    }
+
+                    socket = new DatagramSocket(8888);
+                    socket.setReuseAddress(true);
 
                     //创建一个InetAddree
-                    InetAddress serverAddress = InetAddress.getByName(ipaddress);
+                    InetAddress serverAddress = InetAddress.getByName(ipAddress);
                     //String str = editText.getText().toString();  //这是要传输的数据
                     byte data [] = stringToSend.getBytes();  //把传输内容分解成字节
                     //创建一个DatagramPacket对象，并指定要讲这个数据包发送到网络当中的哪个、地址，以及端口号
-                    DatagramPacket packet = new DatagramPacket(data,data.length,serverAddress, Integer.parseInt(portaddress));
+                    DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, portServer);
                     //调用socket对象的send方法，发送数据
                     socket.send(packet);
                     socket.close();
@@ -149,14 +164,14 @@ public class MainActivity extends ActionBarActivity {
 
             @Override
             protected Void doInBackground(Void... params) {
+                String result;
                 try {
                     //创建一个DatagramSocket对象，并指定监听的端口号
-                    DatagramSocket socket=null;
-                    if (socket == null){
-                        socket = new DatagramSocket(null);
-                        socket.setReuseAddress(true);
-                        socket.bind(new InetSocketAddress(Integer.parseInt(portaddress2)));
-                    }
+                    //DatagramSocket socket=null;
+                    socket = new DatagramSocket(null);
+                    //  socket.setReuseAddress(true);
+                    socket.bind(new InetSocketAddress(portLocal));
+
                     //DatagramSocket socket = new DatagramSocket(4567);
 
                     byte data [] = new byte[4096];
@@ -168,11 +183,12 @@ public class MainActivity extends ActionBarActivity {
                     //使用receive方法接收客户端所发送的数据，
 
                     //如果客户端没有发送数据，该进程就停滞在这里
-                    for (int i = 0; i < 10; i++) {
+                    // for (int i = 0; i < 10; i++) {
                         socket.receive(packet);
                         result = new String(packet.getData(),packet.getOffset(), packet.getLength());
+                    result += "\n";
                         publishProgress(result);
-                    }
+                    //}
                     socket.close();
 
                     //System.out.println("result--->" + result);
